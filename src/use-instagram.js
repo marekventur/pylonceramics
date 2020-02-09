3f17bd90f77616dce5f203e27f70dbc06ab28505
Marek Ventur <marekventur@gmail.com>
2020-02-09 17:10:22 +0000

    Use https, doh

--- src/use-instagram.js
+++ src/use-instagram.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const useInstagram = () => {
     const [posts, setPosts] = useState(null);
     useEffect(() => {
-      fetch('http://server.marekventur.com/pcig')
+      fetch('https://server.marekventur.com/pcig')
         .then(r => r.json())
         .then(({ user }) => {
           const posts = user.edge_owner_to_timeline_media.edges.map(d => d.node);
--- src/use-text-content.js
+++ src/use-text-content.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const useTextContent = () => {
     const [data, setData] = useState(null);
     useEffect(() => {
-      fetch('http://server.marekventur.com/pcgd')
+      fetch('https://server.marekventur.com/pcgd')
         .then(r => r.json())
         .then(setData, setData)  
     }, []);
