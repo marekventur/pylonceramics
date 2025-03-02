7847bb718d66458e667c3f4cec9897b4de611e9f
Marek <marekli-ventur@Mareks-MacBook-Air.local>
2025-03-02 01:41:52 +0000

    fix bug

--- scripts/update-instagram.py
+++ scripts/update-instagram.py
@@ -42,9 +42,9 @@ for post in merged_posts:
     if (image_url.startswith('https:')):
         print ('downloading image %s' % image_url)
         response = requests.get(image_url)
-        file = f'instagram/{post["id"]}.jpg'
+        file = f'public/instagram/{post["id"]}.jpg'
         with open(file, 'wb') as f:
-            f.write("public/" + response.content)
+            f.write(response.content)
         post['media_url'] = file
         
 
